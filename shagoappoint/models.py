033fedd87c539a8f45be5e5812857d0cd2f0367f
hexor <rahulmsharma13@gmail.com>
2020-08-28 20:52:07 +0530

    fixed submit spelling

--- shagoappoint/models.py
+++ shagoappoint/models.py
@@ -6,8 +6,8 @@ class appointment(models.Model):
 
     username = models.CharField(max_length=50)
     contact_no = models.CharField(max_length=10)
+    worker = models.CharField(max_length=2)
     #OTP = models.CharField(max_length=4)
     date = models.DateField()
     alloted_time = models.PositiveSmallIntegerField(default=0)
     alloted_duration = models.PositiveSmallIntegerField(default=0)
-    worker = models.CharField(max_length=2)
